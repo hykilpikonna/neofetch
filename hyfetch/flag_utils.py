4bf185020bf48ee411de74632129d58067e3fcb7
j4d35t0n3 <94765041+j4d35t0n3@users.noreply.github.com>
2023-09-14 09:31:40 -0500

    Renamed to flag_utils

--- hyfetch/flag_utils.py
+++ hyfetch/flag_utils.py
@@ -3,7 +3,13 @@ import numpy as np
 import os
 
 
-def get_array(flag: str, x_len: int, y_len: int) -> np.array:
+def get_flags() -> list:
+    files = os.listdir('hyfetch/flags')
+    files_no_ext = [f.split('.')[0] for f in os.listdir('hyfetch/flags')]
+    return files_no_ext
+
+
+def get_array(flag: str, x_len: int, y_len: int, rotation: int = 0) -> np.array:
     """
     Open the flag file, resize it, and return it as an array of size y * x * 3
     """
@@ -21,8 +27,8 @@ def get_array(flag: str, x_len: int, y_len: int) -> np.array:
 
     filename = files[index]
     im = Image.open('hyfetch/flags/' + filename).convert('RGB')
+    im = im.rotate(rotation, expand=True)
     # +1 to account for newlines
     im = im.resize((x_len + 1, y_len), resample=Image.Resampling.NEAREST)
     arr = np.array(im)
-    print(arr.shape)
     return arr
