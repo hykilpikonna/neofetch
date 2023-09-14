51a0a12da61d711c3ba4f29b7dbc4979d69f9d03
j4d35t0n3 <94765041+j4d35t0n3@users.noreply.github.com>
2023-09-14 09:31:35 -0500

    Added image utilities

---
+++ hyfetch/image_util.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import numpy as np
+import os
+
+
+def get_array(flag: str, x_len: int, y_len: int) -> np.array:
+    """
+    Open the flag file, resize it, and return it as an array of size y * x * 3
+    """
+
+    # Get files in the flag directory
+    files = os.listdir('hyfetch/flags')
+    # Remove file extensions
+    files_no_ext = [f.split('.')[0] for f in os.listdir('hyfetch/flags')]
+
+    # Get index of file in list of files without extensions, raise error if it doesn't exist
+    try:
+        index = files_no_ext.index(flag)
+    except ValueError:
+        raise FileNotFoundError(f'{flag} flag does not exist')
+
+    filename = files[index]
+    im = Image.open('hyfetch/flags/'+filename).convert('RGB')
+    # +1 to account for newlines
+    im = im.resize((x_len+1, y_len), resample=Image.Resampling.NEAREST)
+    arr = np.array(im)
+    print(arr.shape)
+    return arr
