37742d733617afb6b0f3f65978f725076e75d6b7
j4d35t0n3 <94765041+j4d35t0n3@users.noreply.github.com>
2023-09-14 09:31:36 -0500

    Minor changes

--- hyfetch/image_util.py
+++ hyfetch/image_util.py
@@ -20,9 +20,9 @@ def get_array(flag: str, x_len: int, y_len: int) -> np.array:
         raise FileNotFoundError(f'{flag} flag does not exist')
 
     filename = files[index]
-    im = Image.open('hyfetch/flags/'+filename).convert('RGB')
+    im = Image.open('hyfetch/flags/' + filename).convert('RGB')
     # +1 to account for newlines
-    im = im.resize((x_len+1, y_len), resample=Image.Resampling.NEAREST)
+    im = im.resize((x_len + 1, y_len), resample=Image.Resampling.NEAREST)
     arr = np.array(im)
     print(arr.shape)
     return arr
